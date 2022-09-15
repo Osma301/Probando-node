e8608f8de8c5ece6cf6626d5a25a6a472b576b20
Osma301 <Osma301@gmail.com>
2022-09-14 21:46:16 -0300

    agg tarea numero 1 del mejor bootcamp

---
+++ tarea1.js
@@ -0,0 +1,12 @@
+const miNombre = "osmando";
+const miEdad = 21;
+const profesionDev = true;
+
+const fechadeNacimiento = new Date("2000, 12, 20");
+
+const libroFav = {
+    titulo: "Harry Potter",
+    autor: "J. K. Rowling",
+    fecha: "1 de diciembre de 1998",
+    url: "https://www.amazon.com/-/es/J-K-Rowling/dp/8478884459",
+}
